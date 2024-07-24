efcf15989932f540b99f96ddde9ae232e34bc647
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2024-07-24 16:49:01 +0530

    Update day_data.py

--- day_data.py
+++ day_data.py
@@ -32,7 +32,7 @@ try:
     
     # Date Stuff
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(7)
+    twodayago = todayDate - timedelta(30)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
@@ -85,7 +85,7 @@ try:
     
     # Date Stuff
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(7)
+    twodayago = todayDate - timedelta(30)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
