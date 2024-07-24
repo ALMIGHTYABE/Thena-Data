a8b9ec179e8be1df0551f8decc598b186cc21946
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2024-07-24 18:01:15 +0530

    Update pair_data.py

--- pair_data.py
+++ pair_data.py
@@ -23,94 +23,94 @@ def read_params(config_path):
 
 config = read_params(params_path)
 
-# # V1
-# try:
-#     # Params Data
-#     subgraph = config["query"]["subgraph"]
-#     id_data = config["files"]["id_data"]
-#     pair_data_query = config["query"]["pair_data_query"]
-#     epoch_daily_csv = config["files"]["epoch_daily_data"]
-#     pair_data_csv = config["files"]["pair_data"]
-#     provider_url = config["web3"]["provider_url"]
+# V1
+try:
+    # Params Data
+    subgraph = config["query"]["subgraph"]
+    id_data = config["files"]["id_data"]
+    pair_data_query = config["query"]["pair_data_query"]
+    epoch_daily_csv = config["files"]["epoch_daily_data"]
+    pair_data_csv = config["files"]["pair_data"]
+    provider_url = config["web3"]["provider_url"]
 
-#     # Pulling Pair Data
-#     logger.info("Pair Data Started")
+    # Pulling Pair Data
+    logger.info("Pair Data Started")
 
-#     # Request and Edit Pair Data
-#     ids_df = pd.read_csv(id_data)
-#     ids_df = ids_df[ids_df['type'] != "CL"]
+    # Request and Edit Pair Data
+    ids_df = pd.read_csv(id_data)
+    ids_df = ids_df[ids_df['type'] != "CL"]
     
-#     # Today and 2 Day Ago
-#     todayDate = datetime.utcnow()
-#     twodayago = todayDate - timedelta(30)
-#     my_time = datetime.min.time()
-#     my_datetime = datetime.combine(twodayago, my_time)
-#     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
+    # Today and 2 Day Ago
+    todayDate = datetime.utcnow()
+    twodayago = todayDate - timedelta(7)
+    my_time = datetime.min.time()
+    my_datetime = datetime.combine(twodayago, my_time)
+    timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
 
-#     # Web3
-#     validation.METHODS_TO_VALIDATE = []
-#     w3 = Web3(Web3.HTTPProvider(provider_url, request_kwargs={"timeout": 5}))
+    # Web3
+    validation.METHODS_TO_VALIDATE = []
+    w3 = Web3(Web3.HTTPProvider(provider_url, request_kwargs={"timeout": 5}))
 
-#     pairdata_df = pd.DataFrame()
-#     for name, contract_address in zip(ids_df["name"], ids_df["address"]):
-#         try:
-#             pair_data_query["variables"]["pairAddress"] = contract_address
-#             pair_data_query["variables"]["startTime"] = timestamp
-#             response = requests.post(subgraph, json=pair_data_query, timeout=5)
-#             data = response.json()["data"]["pairDayDatas"]
-#             df = pd.json_normalize(data)
-#             df["name"] = name
-#             pairdata_df = pd.concat([pairdata_df, df], axis=0, ignore_index=True)
-#             pairdata_df.reset_index(drop=True, inplace=True)
-#         except Exception as e:
-#             logger.error("Error occurred during Pair Data process. Pair: %s, Address: %s, Error: %s" % (name, contract_address, e))
+    pairdata_df = pd.DataFrame()
+    for name, contract_address in zip(ids_df["name"], ids_df["address"]):
+        try:
+            pair_data_query["variables"]["pairAddress"] = contract_address
+            pair_data_query["variables"]["startTime"] = timestamp
+            response = requests.post(subgraph, json=pair_data_query, timeout=5)
+            data = response.json()["data"]["pairDayDatas"]
+            df = pd.json_normalize(data)
+            df["name"] = name
+            pairdata_df = pd.concat([pairdata_df, df], axis=0, ignore_index=True)
+            pairdata_df.reset_index(drop=True, inplace=True)
+        except Exception as e:
+            logger.error("Error occurred during Pair Data process. Pair: %s, Address: %s, Error: %s" % (name, contract_address, e))
 
-#     epoch_data = pd.read_csv(epoch_daily_csv)
-#     epoch_data["date"] = epoch_data["date"].apply(lambda date: datetime.strptime(date, "%d-%m-%Y").date())
+    epoch_data = pd.read_csv(epoch_daily_csv)
+    epoch_data["date"] = epoch_data["date"].apply(lambda date: datetime.strptime(date, "%d-%m-%Y").date())
 
-#     pairdata_df["date"] = pairdata_df["date"].apply(lambda timestamp: datetime.utcfromtimestamp(timestamp).date())
-#     pairdata_df = pd.merge(pairdata_df, ids_df[["name", "address", "type"]], how="left", on="name")
-#     pairdata_df = pd.merge(pairdata_df, epoch_data[["date", "epoch"]], how="left", on="date")
+    pairdata_df["date"] = pairdata_df["date"].apply(lambda timestamp: datetime.utcfromtimestamp(timestamp).date())
+    pairdata_df = pd.merge(pairdata_df, ids_df[["name", "address", "type"]], how="left", on="name")
+    pairdata_df = pd.merge(pairdata_df, epoch_data[["date", "epoch"]], how="left", on="date")
 
-#     pairdata_df["fee %"] = pairdata_df["type"]
-#     pairdata_df["fee %"].replace({"vAMM": 0.20, "sAMM": 0.01}, inplace=True)
+    pairdata_df["fee %"] = pairdata_df["type"]
+    pairdata_df["fee %"].replace({"vAMM": 0.20, "sAMM": 0.01}, inplace=True)
 
-#     pairdata_df["dailyVolumeUSD"] = pd.to_numeric(pairdata_df["dailyVolumeUSD"])
-#     pairdata_df["fee"] = (pairdata_df["dailyVolumeUSD"] * pairdata_df["fee %"]) / 100
-#     pairdata_df.sort_values("date", ascending=True, inplace=True)
-#     pairdata_df["date"] = pairdata_df["date"].apply(lambda date: datetime.strftime(date, "%Y-%m-%d"))
-#     pairdata_df = pairdata_df[['id', 'date', 'dailyVolumeToken0', 'dailyVolumeToken1', 'dailyVolumeUSD', 'reserveUSD', '__typename', 'name', 'address', 'type', 'epoch', 'fee %', 'fee']]
+    pairdata_df["dailyVolumeUSD"] = pd.to_numeric(pairdata_df["dailyVolumeUSD"])
+    pairdata_df["fee"] = (pairdata_df["dailyVolumeUSD"] * pairdata_df["fee %"]) / 100
+    pairdata_df.sort_values("date", ascending=True, inplace=True)
+    pairdata_df["date"] = pairdata_df["date"].apply(lambda date: datetime.strftime(date, "%Y-%m-%d"))
+    pairdata_df = pairdata_df[['id', 'date', 'dailyVolumeToken0', 'dailyVolumeToken1', 'dailyVolumeUSD', 'reserveUSD', '__typename', 'name', 'address', 'type', 'epoch', 'fee %', 'fee']]
     
-#     pairdata_old = pd.read_csv(pair_data_csv)
-#     drop_index = pairdata_old[pairdata_old['date']>datetime.fromtimestamp(timestamp).strftime(format='%Y-%m-%d')].index
-#     index_list = drop_index.to_list()
-#     index_list = list(map(lambda x: x + 2, index_list))
-#     pairdata_df['__typename'] = 'V1'
-#     df_values = pairdata_df.values.tolist()
+    pairdata_old = pd.read_csv(pair_data_csv)
+    drop_index = pairdata_old[pairdata_old['date']>datetime.fromtimestamp(timestamp).strftime(format='%Y-%m-%d')].index
+    index_list = drop_index.to_list()
+    index_list = list(map(lambda x: x + 2, index_list))
+    pairdata_df['__typename'] = 'V1'
+    df_values = pairdata_df.values.tolist()
 
-#     if pairdata_df.empty:
-#         raise Exception("Dataframe is empty")
+    if pairdata_df.empty:
+        raise Exception("Dataframe is empty")
 
-#     # Write to GSheets
-#     credentials = os.environ["GKEY"]
-#     credentials = json.loads(credentials)
-#     gc = gspread.service_account_from_dict(credentials)
+    # Write to GSheets
+    credentials = os.environ["GKEY"]
+    credentials = json.loads(credentials)
+    gc = gspread.service_account_from_dict(credentials)
 
-#     # Open a google sheet
-#     sheetkey = config["gsheets"]["pair_data_sheet_key"]
-#     gs = gc.open_by_key(sheetkey)
+    # Open a google sheet
+    sheetkey = config["gsheets"]["pair_data_sheet_key"]
+    gs = gc.open_by_key(sheetkey)
 
-#     # Select a work sheet from its name
-#     worksheet1 = gs.worksheet("Master")
-#     if index_list != []:
-#         worksheet1.delete_rows(index_list[0], index_list[-1])
+    # Select a work sheet from its name
+    worksheet1 = gs.worksheet("Master")
+    if index_list != []:
+        worksheet1.delete_rows(index_list[0], index_list[-1])
 
-#     # Append to Worksheet
-#     gs.values_append("Master", {"valueInputOption": "USER_ENTERED"}, {"values": df_values})
+    # Append to Worksheet
+    gs.values_append("Master", {"valueInputOption": "USER_ENTERED"}, {"values": df_values})
 
-#     logger.info("Pair Data Ended")
-# except Exception as e:
-#     logger.error("Error occurred during Pair Data process. Error: %s" % e, exc_info=True)
+    logger.info("Pair Data Ended")
+except Exception as e:
+    logger.error("Error occurred during Pair Data process. Error: %s" % e, exc_info=True)
     
    
 # Fusion
@@ -137,7 +137,7 @@ try:
     
     # Today and 2 Day Ago
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(30)
+    twodayago = todayDate - timedelta(7)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
