fba5b874f71d9db8cf6a86510be42c0bb2b62a40
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2024-12-05 04:40:29 +0530

    day_data_updated

--- day_data.py
+++ day_data.py
@@ -93,7 +93,7 @@ try:
     
     # Date Stuff
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(10)
+    twodayago = todayDate - timedelta(11)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
