a0453fb58bff5239682b8e174aa9f49af8fa6656
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2024-12-08 23:33:41 +0530

    daydelta restored

--- day_data.py
+++ day_data.py
@@ -25,7 +25,7 @@ try:
     
     # Date Stuff
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(40)
+    twodayago = todayDate - timedelta(daydelta)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
@@ -93,7 +93,7 @@ try:
     
     # Date Stuff
     todayDate = datetime.utcnow()
-    twodayago = todayDate - timedelta(40)
+    twodayago = todayDate - timedelta(daydelta)
     my_time = datetime.min.time()
     my_datetime = datetime.combine(twodayago, my_time)
     timestamp = int(my_datetime.replace(tzinfo=timezone.utc).timestamp())
